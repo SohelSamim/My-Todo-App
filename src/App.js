cbe07ca0c5332ff32add2e27bce586f1ca9fa8af
Sohel Samimi <47162719+SohelSamim@users.noreply.github.com>
2020-11-01 02:33:46 +0100

    Created this app on haloween

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
+import { title } from 'process';
 import React, { useState } from 'react';
 import './App.css';
 import Todo from './Todo';
 
 function App() {
   const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos( [...todos, input]);
+    setInput("");
+  };
+
+
+
   return (
     <div className="App"> 
     <h1> Todo App </h1>
-    <input 
+
+    <form> 
+      <input 
     value= {input} 
     onChange= {(e) => setInput(e.target.value)} 
     type="text" 
     />
-    <button> Add todo</button>
-    <Todo title ="Take dogs out 1"/>
-    <Todo title ="Take dogs out 2"/>
-    <Todo title ="Take dogs out 3"/>
-    <Todo title ="Take dogs out 4"/>
-    <Todo title ="Take dogs out 5"/>
-    <Todo title ="Take dogs out 6"/>
+    <button disabled= {!input} type ="submit" onClick={addTodo}>
+      Add todo
+    </button>
+    </form>
+
+    {todos.map((todo, i) =>(
+    <Todo title={todo} key={i} />
+    ))}
+    
+
 
     </div>
   );
