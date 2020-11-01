60e069a0f8c12f7acc918fd644b86c8649fb71f0
Sohel Samimi <47162719+SohelSamim@users.noreply.github.com>
2020-11-01 01:54:40 +0100

    created-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Todo from './Todo';
 
 function App() {
+  const [input, setInput] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <h1> Todo App </h1>
+    <input 
+    value= {input} 
+    onChange= {(e) => setInput(e.target.value)} 
+    type="text" 
+    />
+    <button> Add todo</button>
+    <Todo title ="Take dogs out 1"/>
+    <Todo title ="Take dogs out 2"/>
+    <Todo title ="Take dogs out 3"/>
+    <Todo title ="Take dogs out 4"/>
+    <Todo title ="Take dogs out 5"/>
+    <Todo title ="Take dogs out 6"/>
+
     </div>
   );
 }
---
+++ src/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Todo = (props) => {
+    return (
+        <div>
+            <h1> {props.title}</h1>
+        </div>
+
+
+    );
+};
+
+
+export default Todo;
+
